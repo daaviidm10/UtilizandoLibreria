df100577fac06e12ca36a001b9c47acf206c439a
David Mariño Alonso <dmarinoalonso@danielcastelao.org>
2021-02-25 19:56:43 +0100

    Visualizar consola y ventana.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
+import Visualizar.*;
+
+import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+            VisualizaConsola vc = new VisualizaConsola();
+            vc.visualizar("Hola buenas");
+            VisualizaVentana vv = new VisualizaVentana();
+            vv.visualizar(JOptionPane.showInputDialog("Hola buenaaasss"));
     }
+
 }
